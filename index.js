1bd73c42eca97e8ffb80a21e48dd9357c9fc34f6
Austin Keener <austingraphicandweb@gmail.com>
2020-12-16 09:52:32 -0500

    express skeleton server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+//in this file i am going to create a basic server for my express application. the server will notify me of the port that the server will start on and display a json data file on the front end.
+
+//require express and PORT 3000 and assign express() to a variable (app) to start plus other files that need to be required within the server page
+const express = require("express");
+const PORT = 3000;
+const app = express();
+const data = require("./data/data.json");
+
+//setup express.listen so that I can see that the server is live within my terminal window
+app.listen(PORT, () => {
+  console.log(`Server is live on port ${PORT}!`);
+  console.log(data);
+});
